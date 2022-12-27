963cfd799f2717a1522ddcd8bdd01a71eb1db201
jeerasakcom <jeerasak297@gmail.com>
2022-12-27 11:07:10 +0700

    1000

--- lib/main.dart
+++ lib/main.dart
@@ -94,10 +94,11 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'times:',
+              style: Theme.of(context).textTheme.headline4,
             ),
             Text(
               '$_counter',
